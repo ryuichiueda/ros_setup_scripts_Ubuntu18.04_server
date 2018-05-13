f1b9a297b5f1483b1c867bcd29442e6e2b7d2ec5
Ryuichi Ueda <ryuichiueda@gmail.com>
2018-05-13 11:22:59 +0000

    Add files

---
+++ README.md
@@ -0,0 +1,17 @@
+# ros_setup_scripts_Ubuntu17.04
+
+ROS (Melodic Morenia) setup scripts for Ubuntu 18.04
+
+# scripts
+
+## step0.bash
+
+A script before using this repository. The script can be used by copy and paste.
+
+## step1.bash
+
+It installs ROS. Roscore will run if all procedures in this script are completed.
+
+# tested on ...
+
+* Ubuntu 18.04 Server
---
+++ step1.bash
@@ -0,0 +1,47 @@
+#!/bin/bash -exv
+
+UBUNTU_VER=$(lsb_release -sc)
+ROS_VER=melodic
+[ "$UBUNTU_VER" = "bionic" ] || exit 1
+
+echo "deb http://packages.ros.org/ros/ubuntu $UBUNTU_VER main" > /tmp/$$-deb
+sudo mv /tmp/$$-deb /etc/apt/sources.list.d/ros-latest.list
+
+set +vx
+while ! sudo apt-get install -y curl ; do
+	echo '***WAITING TO GET A LOCK FOR APT...***'
+	sleep 1
+done
+set -vx
+
+curl -k https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
+sudo apt-get update || echo ""
+
+sudo apt-get install -y ros-${ROS_VER}-ros-base
+
+ls /etc/ros/rosdep/sources.list.d/20-default.list && sudo rm /etc/ros/rosdep/sources.list.d/20-default.list
+sudo rosdep init 
+rosdep update
+
+sudo apt-get install -y python-rosinstall
+sudo apt-get install -y build-essential
+
+grep -F "source /opt/ros/$ROS_VER/setup.bash" ~/.bashrc ||
+echo "source /opt/ros/$ROS_VER/setup.bash" >> ~/.bashrc
+
+grep -F "ROS_MASTER_URI" ~/.bashrc ||
+echo "export ROS_MASTER_URI=http://localhost:11311" >> ~/.bashrc
+
+grep -F "ROS_HOSTNAME" ~/.bashrc ||
+echo "export ROS_HOSTNAME=localhost" >> ~/.bashrc
+
+
+### instruction for user ###
+set +xv
+
+echo '***INSTRUCTION*****************'
+echo '* do the following command    *'
+echo '* $ source ~/.bashrc          *'
+echo '* after that, try             *'
+echo '* $ LANG=C roscore            *'
+echo '*******************************'
